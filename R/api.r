adcdd3acfb601c28be4fce47b0e437721afa1a98
Robert Zamora <zamora.rr@gmail.com>
2017-05-09 18:12:53 -0400

    removed httr imports

--- R/api.r
+++ R/api.r
@@ -2,31 +2,30 @@
 #'
 #' @param path path to pull
 #' @param query query parameters
-#' @importFrom httr modify_url GET authenticate http_type user_agent content http_error
 msf_api <- function(path, query = NULL) {
   # url
   baseurl <- "https://www.mysportsfeeds.com"
   path <- paste0("api/feed/pull/", path)
-  url <- modify_url(baseurl, path = path, query = query)
+  url <- httr::modify_url(baseurl, path = path, query = query)
 
   # authentication
   username <- Sys.getenv("MYSPORTSFEEDS_USER")
   password <- Sys.getenv("MYSPORTSFEEDS_PASSWORD")
 
   # get data
-  resp <- GET(url, authenticate(username, password))
+  resp <- httr::GET(url, httr::authenticate(username, password))
 
   # get content
-  page <- content(resp, "text", encoding = "utf-8")
+  page <- httr::content(resp, "text", encoding = "utf-8")
 
   # check errors
-  if (http_error(resp)) {
+  if (httr::http_error(resp)) {
     html <- xml2::read_html(page)
     error_msg <- xml2::xml_text(xml2::xml_find_all(html, "//body/h1"))
     stop(
       sprintf(
         "MySportsFeeds API request failed [%s]\n%s\n",
-        status_code(resp),
+        htrr::status_code(resp),
         error_msg
       ),
       call. = FALSE
@@ -34,7 +33,7 @@ msf_api <- function(path, query = NULL) {
   }
 
   # check data type
-  if (http_type(resp) != "application/json") {
+  if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
 
