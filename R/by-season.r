484569bba96bbe9378376dae01aa07ee72c68926
Robert Zamora <zamora.rr@gmail.com>
2017-08-24 23:35:02 -0400

    refactored season queries

---
+++ R/by-season.r
@@ -0,0 +1,48 @@
+#' MySportsFeeds API calls by season
+#'
+#' @param req data to request
+#' @param sport mlb | nfl | nba | etc..
+#' @param season ex. 2017-regular | current
+msf_by_season <- function(req, sport, season) {
+  path <- paste0(c(sport, season, req), collapse = "/")
+  msf_api(path)
+}
+
+#' Full Game Schedule
+#'
+#' A list of all games to be played for the entire season.
+#' @inheritParams msf_by_season
+full_game_schedule <- function(sport, season = "current") {
+  msf_by_season("full_game_schedule.json", sport, season)
+}
+
+#' List of active players
+#' @inheritParams msf_by_season
+active_players <- function(sport, season = "current") {
+  msf_by_season("active_players.json", sport, season)
+}
+
+#' Player Game Logs
+#'
+#' Game logs for one or more players
+#' @inheritParams msf_by_season
+player_gamelogs <- function(sport, season = "current") {
+  # need to also have a query that specifies the players or games to return
+  msf_by_season("player_gamelogs.json", sport, season)
+}
+
+#' Latest Updates
+#'
+#' This feed lists the last updated date/time for each available feed.
+#' @inheritParams msf_by_season
+latest_updates <- function(sport, season = "current") {
+  msf_by_season("latest_updates.json", sport, season)
+}
+
+#' Cumulative Player Stats
+#'
+#' A list of player stats totals for all roster players, summarized by their latest team.
+#' @inheritParams msf_by_season
+cumulative_player_stats <- function(sport, season = "current") {
+  msf_by_season("cumulative_player_stats.json", sport, season)
+}
--- R/mlb-by-season.r
+++ R/mlb-by-season.r
@@ -1,52 +1,31 @@
-#' MLB Full Game Schedule
-#'
-#' A list of all games to be played for the entire season.
-#' @param season string, ex. "2017-regular" | "2017-playoff"
+#' @rdname full_game_schedule
 #' @export
 mlb_full_game_schedule <- function(season = "current") {
-  path <- paste0(c("mlb", season, "full_game_schedule.json"), collapse = "/")
-  msf_api(path)
+  full_game_schedule("mlb", season)
 }
 
-#' List of active players
-#' @param season ex. 2017-regular
+#' @rdname active_players
 #' @export
 mlb_active_players <- function(season = "current") {
-  path <- paste0(c("mlb", season, "active_players.json"), collapse = "/")
-  msf_api(path)
+  active_players("mlb", season)
 }
 
-#' Player Game Logs
-#'
-#' Game logs for one or more players
-#' @param season ex. 2017-regular
+#' @rdname player_gamelogs
 #' @export
 mlb_player_gamelogs <- function(season = "current") {
-  # need to also have a query that specifies the players or games to return
-  path <- paste0(c("mlb", season, "player_gamelogs.json"), collapse = "/")
-  msf_api(path)
+  player_gamelogs("mlb", season)
 }
 
-#' Latest Updates
-#'
-#' This feed lists the last updated date/time for each available feed.
-#'
-#' @param season ex. 2017-regular
+#' @rdname latest_updates
 #' @export
 mlb_latest_updates <- function(season = "current") {
-  path <- paste0(c("mlb", season, "latest_updates.json"), collapse = "/")
-  msf_api(path)
+  latest_updates("mlb", season)
 }
 
-#' Cumulative Player Stats
-#'
-#' A list of player stats totals for all roster players, summarized by their latest team.
-#'
-#' @param season ex. 2017-regular
+#' @rdname cumulative_player_stats
 #' @export
 mlb_cumulative_player_stats <- function(season = "current") {
-  path <- paste0(c("mlb", season, "cumulative_player_stats.json"), collapse = "/")
-  msf_api(path)
+  cumulative_player_stats("mlb", season)
 }
 
 
