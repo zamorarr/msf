22f29fc4f6870be0e75106895ead565640888210
Robert Zamora <zamora.rr@gmail.com>
2017-08-24 23:34:41 -0400

    refactored gameid queries

--- R/mlb-by-game.r
+++ R/mlb-by-game.r
@@ -1,34 +1,17 @@
-#' MLB Game Boxscore
-#'
-#' A boxscore of scoring plays and player stats, for a game.
-#' @param season string, ex. "2017-regular" | "2017-playoff"
-#' @param gameid string, "40265"
+#' @rdname game_boxscore
 #' @export
 mlb_game_boxscore <- function(gameid, season = "current") {
-  path <- paste0(c("mlb", season, "game_boxscore.json"), collapse = "/")
-  query <- list(gameid = gameid)
-  msf_api(path, query)
+  game_boxscore("mlb", gameid, season)
 }
 
-#' MLB Game Play-by-Play
-#'
-#' All plays that have occurred for a game.
-#' @param season string, ex. "2017-regular" | "2017-playoff"
-#' @param gameid string, "40265"
+#' @rdname game_pbp
 #' @export
 mlb_game_pbp <- function(gameid, season = "current") {
-  path <- paste0(c("mlb", season, "game_playbyplay.json"), collapse = "/")
-  query <- list(gameid = gameid)
-  msf_api(path, query)
+  game_pbp("mlb", gameid, season)
 }
 
-#' Download starting mlb starting lineups
-#'
-#' @param gameid the game id
-#' @param season season such as 2017-regular | 2016-playoffs | current
+#' @rdname game_starting_lineup
 #' @export
 mlb_game_starting_lineup <- function(gameid, season = "current") {
-  path <- paste0(c("mlb", season, "game_startinglineup.json"), collapse = "/")
-  query <- list(gameid = gameid)
-  msf_api(path, query)
+  game_starting_lineup("mlb", gameid, season)
 }
