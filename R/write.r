09b6762912c97719d8763de2454a171f294df1d2
Robert Zamora <zamora.rr@gmail.com>
2017-11-19 23:08:52 -0500

    update documentation

---
+++ R/write.r
@@ -0,0 +1,20 @@
+#' Write MySportsFeeds JSON to disk
+#'
+#' Files will be named by the name variable in the object. Ex. resp$name
+#' @param resp response or list of reponses
+#' @param path path to directory
+#' @export
+write_msf <- function(resp, path) {
+  assertive.files::assert_all_are_dirs(path)
+
+  if (class(resp) == "msf_api") {
+    filename <- paste0(resp$name, ".json")
+    filepath <- file.path(path, filename)
+    jsonlite::write_json(resp$content, filepath)
+  } else {
+    filenames <- purrr::map_chr(resp, "name")
+    filenames <- paste0(filenames, ".json")
+    filepaths <- file.path(path, filenames)
+    purrr::walk2(resp, filepaths, ~ jsonlite::write_json(.x$content, .y))
+  }
+}
